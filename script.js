8f71d5506a1f77bae2c7c1914f7dd01d4b11b256
Draoui-Haroun <draouiharoun63@gmail.com>
2026-06-18 17:56:00 +0100

    Implement image slider functionality

--- script.js
+++ script.js
@@ -1,11 +1,40 @@
-const links = document.querySelectorAll(".menu a");
 
-links.forEach(link => {
-    link.addEventListener("click", () => {
-        link.style.transform = "scale(1.2)";
 
-        setTimeout(() => {
-            link.style.transform = "scale(1)";
-        }, 300);
-    });
+const nextEl = document.querySelector(".next");
+
+const prevEl = document.querySelector(".prev");
+
+const imgsEl = document.querySelectorAll("img");
+
+const imageContainerEl = document.querySelector(".image-container");
+
+let currentImg = 1;
+
+let timeout;
+
+nextEl.addEventListener("click", () => {
+  currentImg++;
+  clearTimeout(timeout);
+  updateImg();
 });
+
+prevEl.addEventListener("click", () => {
+  currentImg--;
+  clearTimeout(timeout);
+  updateImg();
+});
+
+updateImg();
+
+function updateImg() {
+  if (currentImg > imgsEl.length -4) {
+    currentImg = 1;
+  } else if (currentImg < 1) {
+    currentImg = imgsEl.length;
+  }
+  imageContainerEl.style.transform = `translateX(-${(currentImg - 1) * 740}px)`;
+  timeout = setTimeout(() => {
+    currentImg++;
+    updateImg();
+  }, 5000);
+}
